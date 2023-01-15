b8f04c91fa4f0400016c05dd5f1d51e1ede43fc5
黄锦辉 <121326855+cxzcxccd@users.noreply.github.com>
2023-01-15 11:38:46 +0800

    Update 快速排序.cpp

--- 快速排序.cpp
+++ 快速排序.cpp
@@ -1,46 +1,33 @@
-#include<stdio.h>
-void sort(int a[], int l, int r)
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int a[100010];
+void qs(int a[],int l,int r)
 {
-	if (l == r)
-		return;
-	int i = l - 1; int j = r + 1;
-	int x = (a[l] + a[r]) / 2;
-	while (i < j)
-	{
-		do
-		{
-			i++;
-		} while (a[i] < x);
-		do
-		{
-			j--;
-		} while (a[j] > x);
-		int t = 0;
-		if (i < j)
-		{
-			t = a[i];
-			a[i] = a[j];
-			a[j] = t;
-		}
-	}
-	sort(a, l, j);
-	sort(a, j + 1, r);
+    if(l>=r) return;
+    int i=l-1;int j=r+1;
+    int x=(a[l]+a[r])/2;
+    while(i<j)
+    {
+        do
+        {
+            i++;
+        }while(a[i]<x);
+        do
+        {
+            j--;
+        }
+        while(a[j]>x);
+        if(i<j) swap(a[i],a[j]);
+    }
+ qs(a,l,j);qs(a,j+1,r);
 }
-
 int main()
 {
-	int n;
-	int i;
-	int a[1000000];
-	scanf_s("%d", &n);
-	for (i = 0; i < n; i++)
-	{
-		scanf_s("%d", &a[i]);
-	}
-	sort(a, 0, n - 1);
-	for (i = 0; i < n; i++)
-	{
-		printf("%d ", a[i]);
-	}
-	return 0;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    qs(a,0,n-1);
+    for(int i=0;i<n;i++) printf("%d ",a[i]);
+    return 0;
 }

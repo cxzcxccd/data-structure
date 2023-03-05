7eb73b706a778ce6d2ce9f734d238b52e72fd2a1
黄锦辉 <121326855+cxzcxccd@users.noreply.github.com>
2023-03-05 12:41:15 +0800

    Update 完全背包问题(朴素).cpp

--- 动态规划/背包问题/完全背包问题(朴素).cpp
+++ 动态规划/背包问题/完全背包问题(朴素).cpp
@@ -1,23 +1,28 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
-const int N = 1010;
-int f[N][N];
+const int N=1010;
+
 int v[N],w[N];
+int f[N][N];
+
 int main()
 {
     int n,m;
     cin>>n>>m;
-    for(int i = 1 ; i <= n ;i ++)
+    for(int i=1;i<=n;i++) cin>>v[i]>>w[i];
+    for(int i=1;i<=n;i++)
     {
-        cin>>v[i]>>w[i];
+        for(int j=0;j<=m;j++)
+        {
+            //f[i][j]=f[i-1][j];
+            for(int k=0;k*v[i]<=j;k++)
+            {
+                f[i][j]=max(f[i][j],f[i-1][j-k*v[i]]+k*w[i]);
+            }
+        }
     }
-
-    for(int i = 1 ; i<=n ;i++)
-    for(int j = 0 ; j<=m ;j++)
-    {
-        for(int k = 0 ; k*v[i]<=j ; k++)
-            f[i][j] = max(f[i][j],f[i-1][j-k*v[i]]+k*w[i]);
-    }
-
-    cout<<f[n][m]<<endl;
+    cout<<f[n][m];
+    return 0;
+    
 }

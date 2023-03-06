306b3eb08f393905d7d8826c5989792f61e84f17
黄锦辉 <121326855+cxzcxccd@users.noreply.github.com>
2023-03-06 23:05:52 +0800

    Create 数字三角形.cpp

---
+++ 动态规划/线性DP/数字三角形.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+const int N=550,INF=-1e9;
+int a[N][N],n;
+int f[N][N];
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=i+1;j++)
+        {
+            f[i][j]=INF;
+        }
+    }
+
+    f[1][1]=a[1][1];
+
+    for(int i=2;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            f[i][j]=max(f[i-1][j-1]+a[i][j],f[i-1][j]+a[i][j]);
+        }
+    }
+
+    int res=INF;
+    for(int i=1;i<=n;i++) res=max(res,f[n][i]);
+    cout<<res;
+
+    return 0;
+}
